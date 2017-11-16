80ca608d39ad5d1e203e25dacbeec1ffb8c0cd2d
Nabila Ahmed <nabila.ahmed63@gmail.com>
2017-11-16 19:01:28 +0200

    :construction: shooting

--- Assets/PlayerControls.cs
+++ Assets/PlayerControls.cs
@@ -54,6 +54,11 @@ public class PlayerControls : MonoBehaviour {
 		RaycastHit hit;
 		if (Physics.Raycast (fpsCam.transform.position, fpsCam.transform.forward, out hit, range)) {
 			Debug.Log (hit.transform.name);
+			Target target = hit.transform.GetComponent<Target> ();
+			if (target != null && 
+				((hit.transform.CompareTag("Bacteria") && weapon1.activeInHierarchy) || (hit.transform.CompareTag("Germ") && weapon2.activeInHierarchy) || (hit.transform.CompareTag("Metal") && weapon3.activeInHierarchy))){
+					target.TakeDamage (damage);
+			}
 		}
 
 	}
