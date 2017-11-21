4551bf578c1138d5a8ab5609b10e32a1813a3136
Hisham Zahran <hmzahran@gmail.com>
2017-11-21 19:38:35 +0200

    :construction: Main Menu Update

---
+++ Assets/Scripts/ButtonManager.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ButtonManager : MonoBehaviour {
+
+    public GameObject moreInfo,credits;
+    private bool showMoreInfo = false;
+	void Awake()
+    {
+        DontDestroyOnLoad(this);
+    }
+
+    public void StartGame()
+    {
+        SceneManager.LoadScene("Game");
+    }
+
+    public void MoreInfo()
+    {
+        showMoreInfo = !showMoreInfo;
+        moreInfo.SetActive(showMoreInfo);
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
